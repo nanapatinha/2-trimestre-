8c1ab2c5f7a9fe511c7cf3893cbe2c1e6bb8f986
nanapatinha <117641525+nanapatinha@users.noreply.github.com>
2024-05-28 08:48:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const caixaPrincipal= document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(."caixa-perguntas");
+const caixaAlternativas= document.querySelector(."ciaxa-alternativas");
+const caixaResultado= document.querySelector(."caixa-resultado")
+const textoResultado= document.querySelector(."texto-resultado");
 
+const perguntas=[
+  {
+    enunciado:"Você prefere comer"
+      alternativas:[
+  "pizza";
+"hamburguer";
+]
+},
+{
+    enunciado:"Qual o melhor lugar para comer?"
+      alternativas:[
+  "Em casa";
+"Na lanchonete/restaurante";
+]
+},
+{
+    enunciado:"Qual o melhor refrigerante"
+      alternativas:[
+  "Coca-cola";
+"Pepsi";
+]
+},
+{
+    enunciado:"Qual é o melhor"
+      alternativas:[
+  "Nescau";
+"Toddy";
+]
+},
+{
+    enunciado:"Você prefere comer "
+      alternativas:[
+  "Legumes e verduraras";
+"Frituras e gosduras";
+]
+},
