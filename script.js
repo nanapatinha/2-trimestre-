a98c6152e79c5d1f2b54844f8f27fca97fea95ac
nanapatinha <117641525+nanapatinha@users.noreply.github.com>
2024-06-19 08:48:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -47,8 +47,13 @@ let perguntaAtual;
 let historiaFinal=" ";
 
 function mostraPergunta() {
+if(atual >= perguntas.lenght){
+mostraResultado();
+return;
+} 
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+  caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
@@ -62,8 +67,14 @@ function mostraAlternativas() {
 }
 function respostaSelecionada(opcaoSelecionada){
   const afirmacoes = opcaoSelecionada.afirmacoes;
-  historiaFinal = afirmacoes;
+  historiaFinal += afirmacoes +"";
   atual++;
   mostraPergunta();
 }
+
+function mostraResultado(){
+  caixaPerguntas.textContent="";
+}
+
+
 mostraPergunta();
